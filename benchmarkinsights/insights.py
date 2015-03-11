38910e0fd3ad4474883bd2eb5b21e35248f34cd2
ken koontz <kenneth.koontz@gmail.com>
2015-03-11 10:36:26 -0600

    add FacebookInsight class

--- benchmarkinsights/insights.py
+++ benchmarkinsights/insights.py
@@ -1,3 +1,18 @@
-class Insight(object):
-    def __init__(self):
-        pass
+class FacebookInsight(object):
+    def __init__(self, data):
+        self.data = data
+
+    @property
+    def likes(self):
+        return {'average': 0,
+                'total': 0}
+
+    @property
+    def checkins(self):
+        return {'average': 0,
+                'total': 0}
+
+    @property
+    def talking_about(self):
+        return {'average': 0,
+                'total': 0}
