6f566b22658668114149cc89244a136bcb87b70e
ken koontz <kenneth.koontz@gmail.com>
2015-03-24 22:39:43 -0600

    models are now dicts

--- benchmarkinsights/insights.py
+++ benchmarkinsights/insights.py
@@ -30,13 +30,13 @@ class FacebookStats(Stats):
 
     @property
     def checkin_count(self):
-        checkin_count = [i.checkin_count for i in self.data]
+        checkin_count = [i.get('checkin_count') for i in self.data]
         return {'average': round(np.average(checkin_count), 1),
                 'total': np.sum(checkin_count)}
 
     @property
     def people_talking(self):
-        people_talking = [i.people_talking for i in self.data]
+        people_talking = [i.get('people_talking') for i in self.data]
         return {'average': round(np.average(people_talking), 1),
                 'total': np.sum(people_talking)}
 
@@ -44,12 +44,12 @@ class FacebookStats(Stats):
 class YelpStats(Stats):
     @property
     def rating(self):
-        rating = [i.rating for i in self.data]
+        rating = [i.get('rating') for i in self.data]
         return {'average': np.average(rating)}
 
     @property
     def rating_count(self):
-        rating_count = [i.rating_count for i in self.data]
+        rating_count = [i.get('rating_count') for i in self.data]
         return {'average': round(np.average(rating_count), 1),
                 'total': np.sum(rating_count)}
 
@@ -57,12 +57,12 @@ class YelpStats(Stats):
 class GoogleStats(Stats):
     @property
     def rating(self):
-        rating = [i.rating for i in self.data]
+        rating = [i.get('rating') for i in self.data]
         return {'average': round(np.average(rating), 1)}
 
     @property
     def rating_count(self):
-        rating_count = [i.rating_count for i in self.data]
+        rating_count = [i.get('rating_count') for i in self.data]
         return {'average': round(np.average(rating_count), 1),
                 'total': np.sum(rating_count)}
 
@@ -70,29 +70,29 @@ class GoogleStats(Stats):
 class FoursquareStats(Stats):
     @property
     def rating(self):
-        rating = [i.rating for i in self.data]
+        rating = [i.get('rating') for i in self.data]
         return {'average': round(np.average(rating), 1)}
 
     @property
     def checkin_count(self):
-        checkin_count = [i.checkin_count for i in self.data]
+        checkin_count = [i.get('checkin_count') for i in self.data]
         return {'average': round(np.average(checkin_count), 1),
                 'total': np.sum(checkin_count)}
 
     @property
     def likes(self):
-        likes = [i.likes for i in self.data]
+        likes = [i.get('likes') for i in self.data]
         return {'average': round(np.average(likes), 1),
                 'total': np.sum(likes)}
 
     @property
     def user_count(self):
-        user_count = [i.user_count for i in self.data]
+        user_count = [i.get('user_count') for i in self.data]
         return {'average': round(np.average(user_count), 1),
                 'total': np.sum(user_count)}
 
     @property
     def tip_count(self):
-        tip_count = [i.tip_count for i in self.data]
+        tip_count = [i.get('tip_count') for i in self.data]
         return {'average': round(np.average(tip_count), 1),
                 'total': np.sum(tip_count)}
